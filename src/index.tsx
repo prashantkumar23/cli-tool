f5263783867fac1d89358489acf6f62c867f0514
Prashant <blackhole.gamma@gmail.com>
2021-03-11 16:11:00 +0530

    inital commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import ReactDOM from "react-dom";
+import CodeCell from "./components/CodeCell";
+
+const App = () => {
+  return (
+    <div>
+      <CodeCell />
+      <CodeCell />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
